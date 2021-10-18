b76e7d017424e97e89a8310871231f5a15163996
Shuchita Taneja <shtaneja@microsoft.com>
2021-10-18 22:34:13 +1100

    approval

---
+++ environment/test/main.tf
@@ -0,0 +1,55 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# TF Backend
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "storagetfstatetest"
+    container_name       = "tfstatecontainer"
+    key                  = "terraform.tfstate"
+  }
+}
+
+
+resource "azurerm_resource_group" "test01" {
+  name     = format("rg-%s-workshop", var.environment)
+  location = var.location
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+resource "azurerm_eventhub_namespace" "test01" {
+  name                = format("%s-%s", var.ehnamespacename, var.environment)
+  location            = var.location
+  resource_group_name = azurerm_resource_group.test01.name
+  sku                 = "Standard"
+  capacity            = 1
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+resource "azurerm_eventhub" "test01" {
+  name                = var.ehname
+  namespace_name      = azurerm_eventhub_namespace.test01.name
+  resource_group_name = azurerm_resource_group.test01.name
+  partition_count     = 2
+  message_retention   = 1
+}
---
+++ environment/test/vars.tf
@@ -0,0 +1,20 @@
+variable "environment" {
+  type        = string
+  description = "The environment the resources are deployed to"
+  default     = "test"
+}
+variable "location" {
+  type        = string
+  description = "The location where the resources are deployed to"
+  default     = "australiaeast"
+}
+variable "ehnamespacename" {
+  type        = string
+  description = "The location where the resources are deployed to"
+  default     = "myehnmsp0092"
+}
+variable "ehname" {
+  type        = string
+  description = "The location where the resources are deployed to"
+  default     = "myeh0092"
+}
